2913d8008c5f7d79cf3ab8e5430d8d50323d2c61
jmldennis <jmldennis@gmail.com>
2021-07-24 15:40:18 -0500

    added some comments

--- ngrok.py
+++ ngrok.py
@@ -5,10 +5,12 @@ from time import sleep
 
 
 class ngrok():
+    #Execute NGROK from bash "ngrok http 5000"
     def __init__(self,protocol="http",port="5000"):
         bashCmd = ["ngrok",protocol,port]
         process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
-        
+    
+    #Get NGROK urls from localhost api
     def get_ngrok_urls(self):
         self.urls = []
         self.ngrok_console = 'http://127.0.0.1:4040/api/tunnels'
@@ -17,10 +19,12 @@ class ngrok():
             self.urls.append(tunnel['public_url'])
         return self.urls
 
+#Simple test app
 if __name__ == '__main__':
     print("Starting NGROK",end="",flush=True)
     n = ngrok()
     done = False
+    #Wait for 2 urls http and https
     while not done:
         sleep(1)
         urls = n.get_ngrok_urls()
@@ -29,5 +33,6 @@ if __name__ == '__main__':
             done = True
         else:
             print(".",end="",flush=True)
+    #Print URLs to screen
     for url in urls:
         print(url)
